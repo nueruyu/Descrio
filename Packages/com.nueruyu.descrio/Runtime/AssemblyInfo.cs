e08a74524c3bcaeb9113f1377385ed6eec0287b5
nueruyu <81212943+nueruyu@users.noreply.github.com>
2025-08-14 21:35:59 +0900

    Add AssemblyInfo

---
+++ Packages/com.nueruyu.descrio/Runtime/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Descrio.Editor")]
+[assembly: InternalsVisibleTo("Descrio.Tests")]
+[assembly: InternalsVisibleTo("Descrio.EditorTests")]
